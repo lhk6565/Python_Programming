7b00526df827ab15f00f82f750421a37ae58414a
HyungKwon Lee <lhk6565@naver.com>
2022-09-23 16:02:33 +0900

    Complete ch04

---
+++ Part 1/Chapter 04/example_if-else3.py
@@ -0,0 +1,10 @@
+num = int(input('정수를 입력하시오: '))
+
+if num < 0:
+    print('음수입니다.')
+else:
+    print('양수입니다.')
+    if num % 2 == 0:
+        print('짝수입니다.')
+    else:
+        print('홀수입니다.')
---
+++ Part 1/Chapter 04/example_if.py
@@ -0,0 +1,5 @@
+score = int(input('점수를 입력하시오: '))
+if score >= 90:
+    print('축하합니다.')
+    print('합격입니다.')
+    print('장학급도 받을 수 있습니다.')
